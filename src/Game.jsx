473b21a7f5826050cb8c85bdfcbf4bd978f3c5c8
KarlVand <cg.vandooren.karl@gmail.com>
2024-10-14 18:28:22 +0200

    functional game

--- src/Game.jsx
+++ src/Game.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import "./index.css";
 import Styles from "./App.module.css";
 
@@ -11,31 +11,64 @@ import rock2 from "./assets/images/rock2.webp";
 import paper2 from "./assets/images/paper2.webp";
 import scissors2 from "./assets/images/scissors2.webp";
 
+/* ------------------------------------------------------------------------ GAME FUNCTION ----- */
 function Game() {
   const [selectedImage, setSelectedImage] = useState(null);
-
   const [computerImage, setComputerImage] = useState(null);
-  const computerChoice = [rock2, paper2, scissors2];
+  const [result, setResult] = useState(null);
+  const playerScore = useRef(0);
+  const computerScore = useRef(0);
+  const computerChoices = [rock2, paper2, scissors2];
 
-  const generateComputerChoice = () => {
+  /* ----- COMPUTER CHOICE ----- */
+  const computerChoice = () => {
     const randomIndex = Math.floor(Math.random() * 3);
-    const randomImage = computerChoice[randomIndex];
+    const randomImage = computerChoices[randomIndex];
     setComputerImage(
       <img
         src={randomImage}
-        alt="Computer's choice"
+        alt={
+          randomImage === rock2
+            ? "Rock"
+            : randomImage === paper2
+            ? "Paper"
+            : "Scissors"
+        }
       />
     );
+    return randomImage;
   };
 
-  const handlePlayerChoice = choice => {
+  /* ----- PLAYER CHOICE ----- */
+  const playerChoice = choice => {
     setSelectedImage(
       <img
         src={choice}
         alt={choice === rock ? "Rock" : choice === paper ? "Paper" : "Scissors"}
       />
     );
-    generateComputerChoice();
+    const compChoice = computerChoice();
+    updateScore(choice, compChoice);
+  };
+  /* -----  SCORE ----- */
+  const updateScore = (playerChoice, computerChoice) => {
+    if (
+      (playerChoice === rock && computerChoice === scissors2) ||
+      (playerChoice === paper && computerChoice === rock2) ||
+      (playerChoice === scissors && computerChoice === paper2)
+    ) {
+      playerScore.current += 1;
+      setResult("You win!");
+    } else if (
+      (playerChoice === rock && computerChoice === paper2) ||
+      (playerChoice === paper && computerChoice === scissors2) ||
+      (playerChoice === scissors && computerChoice === rock2)
+    ) {
+      computerScore.current += 1;
+      setResult("Computer wins!");
+    } else {
+      setResult("Tie!");
+    }
   };
 
   return (
@@ -49,7 +82,9 @@ function Game() {
         <div className={Styles.scoreContainer}>
           <div className={Styles.score}>
             <p>Score</p>
-            <p></p>
+            <p>{playerScore.current}</p>
+            <p>{computerScore.current}</p>
+            <p>{result}</p>
           </div>
         </div>
 
@@ -59,17 +94,17 @@ function Game() {
       <div className={Styles.buttonContainer}>
         <button
           className={Styles.button}
-          onClick={() => handlePlayerChoice(rock)}>
+          onClick={() => playerChoice(rock)}>
           Rock
         </button>
         <button
           className={Styles.button}
-          onClick={() => handlePlayerChoice(paper)}>
+          onClick={() => playerChoice(paper)}>
           Paper
         </button>
         <button
           className={Styles.button}
-          onClick={() => handlePlayerChoice(scissors)}>
+          onClick={() => playerChoice(scissors)}>
           Scissors
         </button>
       </div>
